cc9f2ecd18be191589caa1d5e34cf58f35464e61
Artan <artanda.sd@icloud.com>
2026-04-19 00:36:15 +0200

    Update site

--- docs/assets/js/layers/website/system/firebase-config.js
+++ docs/assets/js/layers/website/system/firebase-config.js
@@ -31,13 +31,13 @@
      03) FIREBASE CONFIGURATION
   ============================================================================= */
   const firebaseConfig = {
-    apiKey: 'AIzaSyDyts8mOZou6K1qPLYR2AQugR0RFwyDS8s',
-    authDomain: 'artan-c7554.firebaseapp.com',
-    projectId: 'artan-c7554',
-    storageBucket: 'artan-c7554.firebasestorage.app',
-    messagingSenderId: '125487592764',
-    appId: '1:125487592764:web:734f02aec52b6bac4c9a39',
-    measurementId: 'G-JJWZXLBF8B'
+    apiKey: 'AIzaSyAogPapUYgtIa2YRO26qRoFy2uWJwBjoM4',
+    authDomain: 'neuroartan-core.firebaseapp.com',
+    projectId: 'neuroartan-core',
+    storageBucket: 'neuroartan-core.firebasestorage.app',
+    messagingSenderId: '323015575722',
+    appId: '1:323015575722:web:8bc8b9d7cf61a1830ea8d3',
+    measurementId: 'G-V1XX390SR4'
   };
 
   /* =============================================================================
